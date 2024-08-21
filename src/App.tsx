96b1289abde794ad2fb39a44e386aea81811702b
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-08-21 07:50:37 +0100

    initial

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className='container flex flex-col items-center p-6'>
+      <h1 className='text-xl font-semibold'>Resto Yassir</h1>
+    </main>
+  );
+}
