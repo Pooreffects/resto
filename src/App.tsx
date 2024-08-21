264a2632f7e8e5493fd0f1e632d8c6e9a2200a53
Mohamed Benabdellah <77970397+Pooreffects@users.noreply.github.com>
2024-08-21 20:03:56 +0100

    fix: Yassir logo ref

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <main className='container mx-auto flex flex-col items-center py-8'>
       <div>
-        <img src='/src/assets/yassir-logo.svg' alt='Logo' />
+        <img src='/yassir-logo.svg' alt='Logo' />
         <h1 className='text-slate-50 font-semibold text-2xl xl:text-4xl'>
           Resto Yassir
         </h1>
